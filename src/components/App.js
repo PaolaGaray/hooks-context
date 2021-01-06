63f8da4bd2437d602026de9681fd7baa78ad7baa
Paola Garay <garay.angela@gmail.com>
2021-01-06 10:20:35 -0500

    Add Login feature implemented by usign useState

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../styles/App.css';
+import LoginUseState from './LoginUseState';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginUseState />
+    </div>
+  );
+}
+
+export default App;
