cf918f98e8dc2b21f5879875849066777a86be68
Paola Garay <garay.angela@gmail.com>
2021-01-06 18:54:18 -0500

    Add login feature implemented with useContext

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import React from 'react';
 import '../styles/App.css';
 //import LoginUseState from './LoginUseState';
 //import LoginUserReducer from './LoginUseReducer/LoginUserReducer'
-import LoginUseReducerImmer from './LoginUseReducerImmer/LoginUseReducerImmer'
+//import LoginUseReducerImmer from './LoginUseReducerImmer/LoginUseReducerImmer';
+import LoginWithContext from './LoginWithContext/LoginWithContext'
 
 function App() {
   return (
     <div className="App">
       {/* <LoginUseState /> */}
       {/* <LoginUserReducer /> */}
-      <LoginUseReducerImmer />
+      {/* <LoginUseReducerImmer /> */}
+      <LoginWithContext />
     </div>
   );
 }
---
+++ src/components/LoginWithContext/reducer.js
@@ -0,0 +1,39 @@
+export default  function loginReducer(draft, action) {
+    switch (action.type) {
+      case 'field': {
+        draft[action.fieldName] = action.payload;
+        return;
+      }
+      case 'login': {
+        draft.error = '';
+        draft.isLoading = true;
+        return;
+      }
+      case 'success': {
+        draft.isLoggedIn = true;
+        draft.isLoading = false;
+        draft.username = '';
+        draft.password = '';
+        return;
+      }
+      case 'error': {
+        draft.error = 'Incorrect username or password!';
+        draft.isLoggedIn = false;
+        draft.isLoading = false;
+        draft.username = '';
+        draft.password = '';
+        return;
+      }
+      case 'logOut': {
+        draft.isLoggedIn = false;
+        return;
+      }
+      case 'toggleTodoCompleted': {
+        const todo = draft.todos.find((item) => item.title === action.payload);
+        todo.completed = !todo.completed;
+        return;
+      }
+      default:
+        return;
+    }
+  }
